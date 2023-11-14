2dbcb3d2eeaafbac3b62bd333f7b500bf30635e2
xtern0o <dos30028gmail.com>
2023-11-15 00:37:25 +0300

    [L7 #1] 2

--- lyceum/users/forms.py
+++ lyceum/users/forms.py
@@ -42,6 +42,11 @@ class ProfileEditForm(django.forms.ModelForm):
 
 
 class UserEditForm(django.forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs["class"] = "form-control"
+
     class Meta:
         model = users.models.ProxyUser
         fields = (
